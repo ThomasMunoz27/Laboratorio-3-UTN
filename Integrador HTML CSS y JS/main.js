51d878a2f028bdb0836f4310d1dbcec105294029
ThomasMunoz27 <thomasnm2004@gmail.com>
2024-09-19 10:55:01 -0300

    integrador no terminado

--- Integrador HTML CSS y JS/main.js
+++ Integrador HTML CSS y JS/main.js
@@ -1,5 +1,9 @@
 const sectionsContainer = document.getElementById("sectionsContainer");
 
+const allProductsSection = document.getElementById("allProductsSection");
+const innerAllProductsSection = document.getElementById("innerAllProductsSection");
+
+
 const burgerSection = document.getElementById("burgerSection");
 const innerBurgerSection = document.getElementById("innerBurgerSection");
 
@@ -15,13 +19,38 @@ const hihgPriceSection = document.getElementById("hihgPriceSection");
 const innerHihgPriceSection = document.getElementById("innerHihgPriceSection");
 
 const lowerPriceSection = document.getElementById("lowerPriceSection");
-const innerLowerPriceSection =document.getElementById("innerLowerPriceSection");
+const innerLowerPriceSection = document.getElementById("innerLowerPriceSection");
+
+
+const popupContainer = document.getElementById("popupContainer")
+
+const updatePopUpContainer = document.getElementById("updatePopUpContainer")
+
+const updateInputName = document.getElementById("updateInputName")
+const updateInputImg = document.getElementById("updateInputImg")
+const updateInputPrice = document.getElementById("updateInputPrice")
+const updateSelectCategory = document.getElementById("updateSelectCategory")
 
 const cancelButton = document.getElementById("cancelButton");
 
+const upDateCancelButton = document.getElementById("upDateCancelButton");
+
 const acceptButton = document.getElementById("acceptButton");
 
+const upDateAcceptButton = document.getElementById("upDateAcceptButton")
+
 const categoriesAside = document. getElementById("categoriesAside");
+
+const searchButton = document.getElementById("searchButton");
+
+const mainBody = document.getElementById("mainBody")
+
+const addElementButton = document.getElementById("addElementButton")
+
+let buttonIdCounter = 1
+
+let visible = "all"
+let priceVisible = ""
 
 class Product {
     img;
@@ -51,18 +80,46 @@ let dictionary ={
 }
 
 
-const addElementButton = document.getElementById("addElementButton")
+
 
 
 //Events Listeners
 
-addElementButton.addEventListener("click", ()=>{
-    const popupContainer = document.getElementById("popupContainer")
 
-    popupContainer.style.display = "flex"
+searchButton.addEventListener("click", ()=>{
+    let arrayAllProducts = [];
+    let arraySearchedProducts = [];
+    const searchBar = document.getElementById("searchBar").value;
 
-
+    for(let key in dictionary){
+        arrayAllProducts = arrayAllProducts.concat(dictionary[key])
+        }
     
+
+    arrayAllProducts.forEach(product => {
+        if(product.title.toLowerCase().includes(searchBar.toLowerCase())){
+            arraySearchedProducts.push(product);
+            renderSearchedProducts(arraySearchedProducts);
+        }
+    })
+})
+
+cancelButton.addEventListener("click", ()=>{
+    popupContainer.style.display = "none"
+    mainBody.classList.remove("blur")
+    resetPopUp();
+})
+
+upDateCancelButton.addEventListener("click", ()=>{
+    updatePopUpContainer.style.display = "none"
+    mainBody.classList.remove("blur")
+    resetPopUp();
+})
+addElementButton.addEventListener("click", ()=>{
+
+
+    popupContainer.style.display = "flex" 
+    mainBody.classList.add("blur")
 })
 
 
@@ -70,14 +127,20 @@ acceptButton.addEventListener("click", ()=>{
     const inputName = document.getElementById("inputName").value;
     const inputImg = document.getElementById("inputImg").value;
     const inputPrice = document.getElementById("inputPrice").value;
-    const selectCategory = document.getElementById("selectCategory").value
+    const selectCategory = document.getElementById("selectCategory").value;
 
     let newProduct = new Product(inputImg, inputName, inputPrice, selectCategory)
-    
+
+    let storedProducts = JSON.parse(localStorage.getItem("products") || "[]");
+
+    storedProducts.push(newProduct)
+
+    localStorage.setItem("products", JSON.stringify(storedProducts))
+
     dictionary[selectCategory].push(newProduct)
 
     renderProducts(dictionary)
-
+    resetPopUp()
 })
 
 
@@ -88,21 +151,39 @@ categoriesAside.addEventListener("click", (event) => {
 
         switch(categoryClicked){
             case "allProductsTitle":
+                visible = "all";
+                priceVisible = "";
+                hihgPriceSection.style.display = "none"
+                lowerPriceSection.style.display = "none"
                 renderProducts(dictionary);
                 break;
             case "burgerTitle":
+                visible = "Hamburguesas"
+                hihgPriceSection.style.display = "none"
+                lowerPriceSection.style.display = "none"
                 renderOneCategory("Hamburguesas");
                 break;
             case "potatoTitle":
+                visible = "Papas"
+                hihgPriceSection.style.display = "none"
+                lowerPriceSection.style.display = "none"
                 renderOneCategory("Papas");
                 break;
             case "sodaTitle":
+                visible = "Gaseosas"
+                hihgPriceSection.style.display = "none"
+                lowerPriceSection.style.display = "none"
                 renderOneCategory("Gaseosas")
                 break
             case "hihgPriceTitle":
+                priceVisible = "High"
+                lowerPriceSection.style.display = "none"
                 orderByHighPrice(dictionary)
                 break;
             case "lowerPriceTitle":
+                priceVisible = "Lower"
+
+                hihgPriceSection.style.display = "none"
                 orderByLowerPrice(dictionary);
                 break;
             default:
@@ -113,12 +194,28 @@ categoriesAside.addEventListener("click", (event) => {
 
 
 
-
+document.addEventListener("DOMContentLoaded", ()=>{
+    loadProductsFromLocalStorage();
+    renderProducts(dictionary);
+})
 
 
 // Funciones
 
+const resetPopUp = () => {
+    const inputName = document.getElementById("inputName");
+    const inputImg = document.getElementById("inputImg");
+    const inputPrice = document.getElementById("inputPrice");
+    const selectCategory = document.getElementById("selectCategory")
+
+    inputName.value = "";
+    selectCategory. value = "Seleccione una categoria"
+    inputImg.value = ""
+    inputPrice.value = 0
+}
+
 function beforeRender(){
+    innerAllProductsSection.innerHTML = ""
     innerBurgerSection.innerHTML = "";
     innerPotatoSection.innerHTML = "";
     innerSodaSection.innerHTML = "";
@@ -130,6 +227,7 @@ function beforeRender(){
 function createProductCard(product){
     const newDiv = document.createElement("div");
     newDiv.classList.add("card__grid")
+    
 
     const newImg = document.createElement("img");
     newImg.src = product.img;
@@ -144,6 +242,38 @@ function createProductCard(product){
     newP2.textContent = `Precio: $${product.price}`;
     newDiv.appendChild(newP2);
 
+
+    
+    newDiv.addEventListener("click", ()=>{
+
+        updatePopUpContainer.style.display = "flex"
+
+        updateInputName.value = product.title;
+        updateInputImg.value = product.img;
+        updateInputPrice.value = product.price;
+
+
+        upDateAcceptButton.addEventListener("click", ()=>{
+            dictionary[product.category].forEach(element =>{
+                if(product.title === element.title){
+                    element.title = updateInputName.value;
+                    element.img = updateInputImg.value;
+                    element.price = updateInputPrice.value;
+                    
+
+                    
+                    
+                }
+                
+                
+            })
+            
+            
+        })
+    });
+
+
+
     return newDiv
 }
 
@@ -152,9 +282,16 @@ function orderByHighPrice(products){
     let arrayHighPrice = [];
 
     // Recorremos todos los productos y los agregamos a un solo array
-    for (let key in products) {
-        arrayHighPrice = arrayHighPrice.concat(products[key]);
+    if(visible === "all"){
+        for (let key in products) {
+                arrayHighPrice = arrayHighPrice.concat(products[key]);
+            }
+    }else{
+        products[visible].forEach(product => {
+                arrayHighPrice.push(product)
+            });
     }
+
 
     // Ordenar los productos por precio de mayor a menor
     arrayHighPrice.sort((a, b) => b.price - a.price);
@@ -168,7 +305,7 @@ function orderByHighPrice(products){
         
         const newDiv = createProductCard(product)
 
-        innerHihgPriceSection.appendChild(newDiv)
+        appendInCorrectSection(newDiv)
                 
         }
 }
@@ -178,9 +315,16 @@ function orderByLowerPrice(products){
     let arrayLowerPrice = [];
 
     // Recorremos todos los productos y los agregamos a un solo array
-    for (let key in products) {
-        arrayLowerPrice = arrayLowerPrice.concat(products[key]);
+    if(visible === "all"){
+        for (let key in products) {
+                arrayLowerPrice = arrayLowerPrice.concat(products[key]);
+            }
+    }else{
+        products[visible].forEach(product => {
+                arrayLowerPrice.push(product)
+            });
     }
+
 
     // Ordenar los productos por precio de mayor a menor
     arrayLowerPrice.sort((a, b) => a.price - b.price);
@@ -194,7 +338,7 @@ function orderByLowerPrice(products){
         
         const newDiv = createProductCard(product)
 
-        innerLowerPriceSection.appendChild(newDiv)
+        appendInCorrectSection(newDiv)
                 
         }
 }
@@ -211,30 +355,20 @@ function renderProducts(products){
                 
                 
                 const newDiv = createProductCard(product)
+                innerAllProductsSection.appendChild(newDiv)
 
-                switch(product.category){
-                    case "Hamburguesas": 
-                        innerBurgerSection.appendChild(newDiv);
-                    break;
-
-                    case "Papas":
-                        innerPotatoSection.appendChild(newDiv);
-                    break
-                    
-                    case "Gaseosas":
-                        innerSodaSection.appendChild(newDiv)
-                    break
-
-                    default:
-                        
-                }
-                    
             }
         }
     }
 }
 
-renderProducts(dictionary)
+function renderSearchedProducts(products){
+    beforeRender();
+    products.forEach(element => {
+        const newDiv = createProductCard(element)
+        innerAllProductsSection.appendChild(newDiv)
+    });
+}
 
 
 function renderOneCategory(category){
@@ -245,27 +379,51 @@ function renderOneCategory(category){
 
             const newDiv = createProductCard(element)
 
-            switch(element.category){
-                case "Hamburguesas": 
-                    innerBurgerSection.appendChild(newDiv);
-                break;
-
-                case "Papas":
-                    innerPotatoSection.appendChild(newDiv);
-                break
-                
-                case "Gaseosas":
-                    innerSodaSection.appendChild(newDiv)
-                break
-
-                default:
-
-                break
-            }
+            appendInCorrectSection(newDiv)
     });
 }
 
 
-cancelButton.addEventListener("click", ()=>{
-    popupContainer.style.display = "none"
-})
+function loadProductsFromLocalStorage(){
+    const productsInLocalStorage = JSON.parse(localStorage.getItem("products") || "[]")
+
+    productsInLocalStorage.forEach(foodProduct =>{
+        console.log(foodProduct.category);
+        
+        dictionary[foodProduct.category].push(foodProduct);
+    })
+}
+
+
+function appendInCorrectSection(divToAppend){
+    switch(visible){
+        case "all":
+            switch(priceVisible){
+                case "":
+                    allProductsSection.style.display = "grid";
+                    innerAllProductsSection.append.appendChild(divToAppend)
+                case "High":
+                    hihgPriceSection.style.display = "grid"
+                    innerHihgPriceSection.appendChild(divToAppend)
+                    break;
+                case "Lower":
+                    lowerPriceSection.style.display = "grid"
+                    innerLowerPriceSection.appendChild(divToAppend)
+                    break;
+            }
+            break;
+        case "Hamburguesas": 
+            innerBurgerSection.appendChild(divToAppend);
+            break;
+        case "Papas":
+            innerPotatoSection.appendChild(divToAppend);
+            break
+        case "Gaseosas":
+            innerSodaSection.appendChild(divToAppend)
+            break
+
+
+        default:
+            break
+    }
+}
